783c81e6b5a3616ca5adfea46f203e3b6fcc4c95
Denis Suwala <denissuwala@wp.pl>
2022-10-17 22:36:49 +0200

    added function to change backoground color

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,12 +1,22 @@
-console.log("Cześć wszystkim, którzy tu zajrzą!");
+{
+    const welcome = () => {
+    console.log("Cześć wszystkim, którzy tu zajrzą!");
+}
 
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".body");
-let theme = document.querySelector(".js-theme");
+const onChangeBackgroundColor = () => {
+    const body = document.querySelector(".body");
+    const theme = document.querySelector(".js-theme");
 
-button.addEventListener ("click", () => {
     body.classList.toggle("dark");
+    theme.innerText = body.classList.contains("dark")? "Jasny" : "Ciemny" ;
+};
 
+welcome();
+    
+ const init = () => {
+    const button = document.querySelector(".js-button");
+    button.addEventListener ("click", onChangeBackgroundColor);
+};
 
-theme.innerText = body.classList.contains("dark")? "Jasny" : "Ciemny" ;
-})
+init();
+};
